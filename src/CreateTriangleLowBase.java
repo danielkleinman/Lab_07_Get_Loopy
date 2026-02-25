6c5ee2e64826b9722ec466cc1adfa01d7a531617
Daniel Kleinman <danielkleinman70@gmail.com>
2026-02-25 11:27:09 -0500

    Added task 5

--- src/CreateTriangleLowBase.java
+++ src/CreateTriangleLowBase.java
@@ -1,7 +1,7 @@
-public class CreateSquareFigure {
+public class CreateTriangleLowBase {
     public static void main(String[] args) {
         for (int r = 0; r < 5; r++) { // Loop for each row
-            for (int c = 0; c < 5; c++) { // Loop for each column
+            for (int c = 0; c <= r; c++) { // Loop for each column, increasing with each row
                 System.out.print("* "); // Print a star followed by a space
             }
             System.out.println(); // Move to the next line after each row
